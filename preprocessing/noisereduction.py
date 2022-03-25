0c641ab81370ff712118894a03ab9afad0e8a552
Elian Mariano <elianmarianogabriel@gmail.com>
2022-03-25 10:10:50 -0300

    Normalization refactor and noise reduction

---
+++ preprocessing/noisereduction.py
@@ -0,0 +1,60 @@
+"""
+    This file abstracts the noisereduce library in order to work well with
+    the rest of the program.
+"""
+
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+import noisereduce as nr
+import sys
+
+def denoise(rate, audio_signal):
+    # Coverts the data to mono
+    if audio_signal.ndim == 2:
+        audio_signal = audio_signal[:, 0]
+
+    # Noise reduced audio
+    return nr.reduce_noise(y=audio_signal, sr=rate)
+
+if __name__ == '__main__':
+    try:
+        samplerate = 0
+        audio_data = []
+
+        noise_reduced_audio = []
+
+        if '-a' in sys.argv:
+            try:
+                audio_path = str(sys.argv[sys.argv.index('-a')+1])
+                samplerate, audio_data = wavfile.read(audio_path)
+
+                noise_reduced_audio = denoise(samplerate, audio_data)
+            except Exception as ex:
+                print("Please inform the audio file")
+                print(ex)
+
+        if '-w' in sys.argv:
+            try:
+                output = str(sys.argv[sys.argv.index('-w')+1])
+                print(noise_reduced_audio)
+                wavfile.write("{filename}.wav".format(filename=output), samplerate, noise_reduced_audio)
+            except Exception:
+                print("Please inform the name of the output file")
+
+        if "-p" in sys.argv and '-a' in sys.argv:
+            # Input audio
+            plt.figure(1)
+            plt.subplot(131)
+            plt.plot(audio_data)
+            plt.ylabel("Wave")
+            #plt.show()
+
+            # Normalized audio
+            plt.figure(1)
+            plt.subplot(133)
+            plt.plot(noise_reduced_audio)
+            plt.ylabel("Wave")
+            plt.suptitle("Noisy audio/Noise reduced audio")
+            plt.show()
+    except Exception as ex:
+        print(ex)
--- preprocessing/normalization.py
+++ preprocessing/normalization.py
@@ -3,51 +3,70 @@ This file only takes the numpy array which represents
 the not normalized audio signal and results in a normalized numpy array. 
 You can either import it to use its functions or you can execute as a main
 file, pass the audio path and see the audio result.
+If the audio is already normalized, this program will take no result.
 FLAGS:
     -p: Plot the input and output audio
+    -w [file_name]: Writes the result into .wav file
+    -a [audio_path]: Selects a audio file
 """
 
-from email.mime import audio
 import numpy as np
 from scipy.io import wavfile
 import matplotlib.pyplot as plt
 import sys
 
-if (__name__ == '__main__'):
-    try:
-        audio_path = str(sys.argv[1])
-        samplerate, audio_data = wavfile.read(audio_path)
+def normalize(audio_signal):
+    ratio = 32767 / np.max(np.abs(audio_signal))
+    normalized_signal = audio_signal
 
-        ratio = 32767 / np.max(np.abs(audio_data))
-        normalized_signal = audio_data
+    for i in range(0, len(normalized_signal)):
+        if (normalized_signal.ndim == 1):
+            normalized_signal[i] = round(normalized_signal[i]*ratio)
+        elif (normalized_signal.ndim == 2):
+            normalized_signal[i, 0] = round(normalized_signal[i, 0]*ratio)
+            normalized_signal[i, 1] = round(normalized_signal[i, 1]*ratio)
+        
+    return normalized_signal
 
-        for i in range(0, len(normalized_signal)):
-            if (normalized_signal.ndim == 1):
-                normalized_signal[i] = round(normalized_signal[i]*ratio)
-            elif (normalized_signal.ndim == 2):
-                normalized_signal[i, 0] = round(normalized_signal[i, 0]*ratio)
-                normalized_signal[i, 1] = round(normalized_signal[i, 1]*ratio)
+if __name__ == '__main__':
+    samplerate = 0
+    audio_data = []
 
-        if ('-p' in sys.argv):
-            time = len(audio_data) / float(samplerate)
-            print(time)
+    normalized_audio = []
 
-            # Input audio
-            plt.figure(1)
-            plt.subplot(131)
-            plt.plot(audio_data)
-            plt.ylabel("Wave")
-            #plt.show()
+    if ('-a' in sys.argv):
+        try:
+            audio_path = str(sys.argv[sys.argv.index('-a')+1])
+            samplerate, audio_data = wavfile.read(audio_path)
 
-            # Normalized audio
-            plt.figure(1)
-            plt.subplot(133)
-            plt.plot(normalized_signal)
-            plt.ylabel("Wave")
-            plt.suptitle("Not normalized/Normalized")
-            plt.show()
+            normalized_audio = normalize(audio_data)
+        except Exception:
+            print("Please inform a audio file")        
 
-    except IndexError:
-        print("Please inform the audio file")
-    except FileNotFoundError:
-        print("Could not find the file")
+    if ('-w' in sys.argv):
+        try:
+            output = str(sys.argv[sys.argv.index('-w')+1])
+            wavfile.write("{filename}.wav".format(filename = output), samplerate, normalized_audio)
+        except Exception:
+            print("Please inform the name of the output file")
+
+    if '-p' in sys.argv and '-a' in sys.argv:
+        #time = len(audio_data) / float(samplerate)
+        #print(time)
+
+        # Input audio
+        plt.figure(1)
+        plt.subplot(131)
+        plt.plot(audio_data)
+        plt.ylabel("Wave")
+        plt.title("Not normalized audio")
+        #plt.show()
+
+        # Normalized audio
+        plt.figure(1)
+        plt.subplot(133)
+        plt.plot(normalized_audio)
+        plt.ylabel("Wave")
+        plt.suptitle("Normalization camparison")
+        plt.title("Normalized audio")
+        plt.show()
