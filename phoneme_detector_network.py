94ac512f425185c3d75bea9a8ae9b8aee1c73475
Elian Mariano <elianmarianogabriel@gmail.com>
2022-04-04 10:02:34 -0300

    Model description

--- phoneme_detector_network.py
+++ phoneme_detector_network.py
@@ -1,3 +1,15 @@
+"""
+Initial model for phoneme detection. It is expected that this model should be able to map the audio
+into a sequence of phonemes correctly. So because the sequence nature of the audio signal, it is
+supposed that a RNN (Recurrent Neural Network) should be use to peform this task.
+Along with that, the audio could transformed into a mel frequency spectogram, a technique which
+allows the audio wave to be represented by a set of image frames. These frames could be fed into
+a CNN (Convolutional Neural Network), and then resulting in a feature map, this feature map will
+then be fed into the RNN, resulting in the probability for each phoneme.
+The resulting probabilities will be compared in order to find the right sequence of phonemes,
+basing on the CTC algorithm.
+"""
+
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
